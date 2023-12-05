2a02ba3096deba93f8c7c1e53a0c2ff589ce95ee
iyume <iyumelive@gmail.com>
2023-12-05 23:54:45 +0800

    add frontend main page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,52 +1,87 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import Greet from "./components/Greet.vue";
+import {
+  NUpload,
+  NUploadDragger,
+  NText,
+  NIcon,
+  NButton,
+  darkTheme,
+  NConfigProvider,
+  NNumberAnimation,
+  NStatistic,
+  StatisticProps,
+} from "naive-ui";
+import { ArchiveOutline as ArchiveIcon } from "@vicons/ionicons5";
+
+type StatisticThemeOverrides = NonNullable<StatisticProps["themeOverrides"]>;
+
+const stat1Overrides: StatisticThemeOverrides = {
+  labelFontSize: "14px",
+  valueFontSize: "34px",
+};
 </script>
 
 <template>
-  <div class="container">
-    <h1>Welcome to Tauri!</h1>
+  <n-config-provider :theme="darkTheme">
+    <div class="container">
+      <div style="height: 10%"></div>
 
-    <div class="row">
-      <a href="https://vitejs.dev" target="_blank">
-        <img src="/vite.svg" class="logo vite" alt="Vite logo" />
-      </a>
-      <a href="https://tauri.app" target="_blank">
-        <img src="/tauri.svg" class="logo tauri" alt="Tauri logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+      <n-text style="margin-bottom: 6px; font-size: 32px"
+        >大脑 MRI 年龄预测</n-text
+      >
+      <n-text style="margin: 8px auto; font-size: 16px">
+        Brain MRI Age Prediction using Resnet Bottleneck
+      </n-text>
+
+      <div style="height: 32px"></div>
+
+      <n-upload
+        accept=".nii"
+        action="/"
+        :max="1"
+        :default-upload="false"
+        style="width: 60%; margin: auto"
+      >
+        <n-upload-dragger>
+          <div style="margin-bottom: 12px">
+            <n-icon size="48" :depth="3">
+              <archive-icon />
+            </n-icon>
+          </div>
+          <n-text style="font-size: 16px">
+            点击或者拖动文件到该区域来上传
+          </n-text>
+        </n-upload-dragger>
+      </n-upload>
+
+      <div style="height: 48px; flex-shrink: 0"></div>
+
+      <n-button type="primary" size="large" style="margin: auto"
+        >预测年龄</n-button
+      >
+
+      <div style="margin: 40px auto">
+        <n-statistic
+          label="你的预测年龄是"
+          tabular-nums
+          :theme-overrides="stat1Overrides"
+        >
+          <n-number-animation
+            ref="numberAnimationInstRef"
+            :from="0"
+            :to="80"
+            :precision="2"
+          />
+          <template #suffix> 岁</template>
+        </n-statistic>
+      </div>
+
+      <div style="flex-grow: 1"></div>
+
+      <n-text>后端状态 {{ "🟢🔴" }}</n-text>
+      <div style="height: 10%"></div>
     </div>
-
-    <p>Click on the Tauri, Vite, and Vue logos to learn more.</p>
-
-    <p>
-      Recommended IDE setup:
-      <a href="https://code.visualstudio.com/" target="_blank">VS Code</a>
-      +
-      <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-      +
-      <a href="https://github.com/tauri-apps/tauri-vscode" target="_blank"
-        >Tauri</a
-      >
-      +
-      <a href="https://github.com/rust-lang/rust-analyzer" target="_blank"
-        >rust-analyzer</a
-      >
-    </p>
-
-    <Greet />
-  </div>
+  </n-config-provider>
 </template>
-
-<style scoped>
-.logo.vite:hover {
-  filter: drop-shadow(0 0 2em #747bff);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #249b73);
-}
-</style>
